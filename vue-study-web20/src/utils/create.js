d5d66891243c2ff80628bfaafba75ef35e226f72
lqy978599280 <978599280@qq.com>
2020-07-01 11:25:06 +0800

    create.js

--- vue-study-web20/src/utils/create.js
+++ vue-study-web20/src/utils/create.js
@@ -7,28 +7,32 @@ export default function create(Component, props) {
   // 实例创建
   // extend方法返回的组件构造函数
   const Ctor = Vue.extend(Component)
-  // console.log(Ctor);
-  const comp = new Ctor({ propsData: props }).$mount()  //$mount()本质上将vdom=》dom
-  // console.log(comp)
+  // or const Ctor = Vue.extend({render:h=>h(Component,{props})})
+  // console.log(Ctor); 
+  const comp = new Ctor({ propsData: props }).$mount() // $mount()本质上将vdom=》dom
   //暗号：村长喊你来搬砖
+  // console.log(comp)
+
 
   // 通过vm.$el获取生成的dom
   document.body.appendChild(comp.$el)
 
   // 删除函数
   // 获取组件实例
-  // const comp = vm.$children[0]
 
   comp.remove = () => {
     document.body.removeChild(comp.$el)
     comp.$destroy()
   }
+  return comp
+}
+
+
+  // const comp = vm.$children[0]
 
   // 方式二：借鸡生蛋
   // const vm = new Vue({
   //   render(h) {
   //     return h(Component, { props })
   //   }
-  // }).$mount()
-  return comp
-}
+  // }).$mount() // $mount()本质上将vdom=》dom
